db48099fc48bc25c855739e4639ed2fbafcc6647
lerato8 <khambulekhanyisile8@gmail.com>
2023-08-01 00:24:54 +0200

    Free(safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -22,13 +22,11 @@ size_t free_listint_safe(listint_t **h)
 		if (diff > 0)
 		{
 			temp = (*h)->next;
-			free(*h);
 			*h = temp;
 			len++;
 		}
 		else
 		{
-			free(*h);
 			*h = NULL;
 			len++;
 			break;
